ca8ed5242c30a88d8d08fd83eda1645e1e461dd8
Patrick Chang <pchang78@gmail.com>
2024-12-25 21:37:41 -0500

    Add links to documentation page

--- includes/admin/documentation-page.php
+++ includes/admin/documentation-page.php
@@ -9,7 +9,14 @@ function notion_forms_documentation_page() {
         <h1>Documentation</h1>
         <p>
             <a href="https://everydaytech.tv/wp/notion-forms/documentation/getting-started/" target="_blank">Getting Started</a>
-
+            <br>
+            <a href="https://everydaytech.tv/wp/notion-forms/documentation/notion-forms/" target="_blank">Notion Forms</a>
+            <br>
+            <a href="https://everydaytech.tv/wp/notion-forms/documentation/confirmation-page/" target="_blank">Confirmation Page</a>
+            <br>
+            <a href="https://everydaytech.tv/wp/notion-forms/documentation/styles/" target="_blank">Styles</a>
+            <br>
+            <a href="https://everydaytech.tv/wp/notion-forms/documentation/settings/" target="_blank">Settings</a>
         </p>
     </div>
     <?php
